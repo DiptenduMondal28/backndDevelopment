cd6d96193a53b90687e945a1fc7ca9db0f12ff94
DiptenduMondal28 <diptendumondal.2801@gmail.com>
2023-05-05 12:20:49 +0530

    add response

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http=require('http')
 
 const server = http.createServer((req,res)=>{
-    console.log("diptendu");
+    console.log(req.url,req.method,req.headers);
+    res.setHeader('content-type','text/html');
+    res.write('html');
+    res.write('<header><title>this is header file</title></header>');
+    res.write('<body><h1>this is body element</h1></body>');
+    res.write('</html>');
+    res.end();
+    //process.exit();
 })
 
 server.listen(4000);
