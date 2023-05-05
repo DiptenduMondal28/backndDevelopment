e9d2ef80942cf33b5f627aa5b7842ddb77e51449
DiptenduMondal28 <diptendumondal.2801@gmail.com>
2023-05-05 16:22:40 +0530

    set diff url and diff response

--- app.js
+++ app.js
@@ -2,13 +2,36 @@ const http=require('http')
 
 const server = http.createServer((req,res)=>{
     console.log(req.url,req.method,req.headers);
-    res.setHeader('content-type','text/html');
-    res.write('html');
-    res.write('<header><title>this is header file</title></header>');
-    res.write('<body><h1>this is body element</h1></body>');
-    res.write('</html>');
-    res.end();
-    //process.exit();
+    if(req.url=='/home'){
+
+        res.setHeader('content-type','text/html');
+        res.write('html');
+        res.write('<header><title>Home page</title></header>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        res.end();
+    }else if(req.url==='/about'){
+        res.setHeader('content-type','text/html');
+        res.write('html');
+        res.write('<header><title>About page</title></header>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        res.end();
+    }else if(req.url=='/node'){
+        res.setHeader('content-type','text/html');
+        res.write('html');
+        res.write('<header><title>Node page</title></header>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+        res.end();
+    }else{
+        res.setHeader('content-type','text/html');
+        res.write('html');
+        res.write('<header><title>wrong url</title></header>');
+        res.write('<body><h1>your url is wrong</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
 })
 
 server.listen(4000);
